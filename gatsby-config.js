f815e3ea94b82ba0acfd33b53bb5b82f43e3742f
ashutosh sharma <ashutoshsharma@ashutoshs-MacBook-Air.local>
2021-09-28 15:52:03 +0530

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Test site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
